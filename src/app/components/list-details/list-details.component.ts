e93a4f8ec2e3ba09df836fe333b315d294ad9e16
GuilhermeDSantos <53098377+GuilhermeDSantos@users.noreply.github.com>
2025-02-01 13:04:45 -0300

    Set dynamic background color for list icons
    Updated the `List` model to include a `color` property and modified the `list-details` component to apply dynamic background colors to list icons using `NgStyle`. Adjusted mock data in the `ListService` to include color values for each list.

--- src/app/components/list-details/list-details.component.ts
+++ src/app/components/list-details/list-details.component.ts
@@ -1,9 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {List} from '../../models/list';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-list-details',
-  imports: [],
+  imports: [
+    NgStyle
+  ],
   templateUrl: './list-details.component.html',
   styleUrl: './list-details.component.css'
 })
--- src/app/services/list/list.service.ts
+++ src/app/services/list/list.service.ts
@@ -7,10 +7,10 @@ import {List} from '../../models/list';
 export class ListService {
 
   private lists: List[] = [
-    { id: '1', name: 'Family', taskCount: 3 },
-    { id: '2', name: 'Work tasks', taskCount: 2 },
-    { id: '3', name: 'Medical', taskCount: 1 },
-    { id: '4', name: 'Shopping', taskCount: 2 }
+    { id: '1', name: 'Family', color: '#ff453a', taskCount: 3 },
+    { id: '2', name: 'Work tasks', color: '#0883fe', taskCount: 2 },
+    { id: '3', name: 'Medical', color: '#ffa00a', taskCount: 1 },
+    { id: '4', name: 'Shopping', color: '#32d05b', taskCount: 2 }
   ]
 
   findAll(): List[] {
