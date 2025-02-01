3f36015268fdc52f2e21bfa8a44b8e5b3ff68090
GuilhermeDSantos <53098377+GuilhermeDSantos@users.noreply.github.com>
2025-02-01 09:31:57 -0300

    Refactored project structure and improved architecture.

---
+++ src/app/components/list-selector/list-selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-selector',
+  imports: [],
+  templateUrl: './list-selector.component.html',
+  styleUrl: './list-selector.component.css'
+})
+export class ListSelectorComponent {
+
+}
